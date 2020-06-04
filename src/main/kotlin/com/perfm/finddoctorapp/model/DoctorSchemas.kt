99ac581871c2843d0adbc1da65305c14ada53e13
thiyagu.anbu <thethiyagu25@gmail.com>
2020-06-04 15:08:31 +0530

    Added DoctorSchemas

---
+++ src/main/kotlin/com/perfm/finddoctorapp/model/DoctorSchemas.kt
@@ -0,0 +1,23 @@
+package com.perfm.finddoctorapp.model
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDate
+import java.util.*
+
+@Document data class Doctor( @Id val id: String,
+                             val firstName : String,
+                             val lastName : String,
+                             val professionalStatement : String,
+                             val practicingFrom: Date,
+                             var specialization: Specialization,
+                             var doctorSpecialization: DoctorSpecialization,
+                             var qualification: Qualification,
+                             var hospitalAffiliation: HospitalAffiliation
+)
+
+data class Specialization(val id : String, val specializationName: String)
+data class DoctorSpecialization(val id:String, val doctorId: String, val specializationId: String)
+data class Qualification(val id:String,val doctorId:String,val qualificationName:String,val institutionName:String,val procurementYear:Date)
+data class HospitalAffiliation(val id:String,val doctorId:String,val hospitalName:String,val city:String,val country:String,val startDate:String,val endDate:String)
+
