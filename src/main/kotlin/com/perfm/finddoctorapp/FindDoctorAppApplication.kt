5b26b94c065b4b6d0bcba2ee8bc2ab7407089bde
thiyagu.anbu <thethiyagu25@gmail.com>
2020-06-04 13:25:43 +0530

    first commit

---
+++ src/main/kotlin/com/perfm/finddoctorapp/FindDoctorAppApplication.kt
@@ -0,0 +1,11 @@
+package com.perfm.finddoctorapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FindDoctorAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<FindDoctorAppApplication>(*args)
+}
---
+++ src/test/kotlin/com/perfm/finddoctorapp/FindDoctorAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.perfm.finddoctorapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FindDoctorAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
