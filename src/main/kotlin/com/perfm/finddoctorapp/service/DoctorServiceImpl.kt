e19fa0a7006b1bc7c9909f2aec90e9f5a51099e7
thiyagu.anbu <thethiyagu25@gmail.com>
2020-06-05 21:57:48 +0530

    Integrated Apache Kafka Publisher and Consumer

--- src/main/kotlin/com/perfm/finddoctorapp/service/DoctorServiceImpl.kt
+++ src/main/kotlin/com/perfm/finddoctorapp/service/DoctorServiceImpl.kt
@@ -2,6 +2,7 @@ package com.perfm.finddoctorapp.service
 
 import com.perfm.finddoctorapp.model.Doctor
 import com.perfm.finddoctorapp.repository.DoctorRepository
+import com.perfm.finddoctorapp.repository.HospitalDetailsRepository
 import com.perfm.finddoctorapp.util.BasicCrud
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class DoctorServiceImpl(val doctorRepository: DoctorRepository):BasicCrud<String, Doctor> {
+class DoctorServiceImpl(val doctorRepository: DoctorRepository, val hospitalDetailsRepository: HospitalDetailsRepository):BasicCrud<String, Doctor> {
     override fun getAll(pageable: Pageable): Page<Doctor> {
         return doctorRepository.findAll(pageable)
     }
@@ -18,9 +19,8 @@ class DoctorServiceImpl(val doctorRepository: DoctorRepository):BasicCrud<String
         return doctorRepository.findById(id)
     }
 
-    override fun insert(obj: Doctor): Doctor {
-        TODO("Not yet implemented")
-    }
+    override fun insert(obj: Doctor): Doctor = doctorRepository.insert(obj.apply { this.hospitalAffiliation.hospitalDetails=hospitalDetailsRepository.findById(obj.hospitalAffiliation.hospitalDetails.id).get()})
+
 
     override fun update(obj: Doctor): Doctor {
         TODO("Not yet implemented")
