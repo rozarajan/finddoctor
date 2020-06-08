a2cf1225af68660a65a799a8744138950d9c80b9
thiyagu.anbu <thethiyagu25@gmail.com>
2020-06-08 15:15:13 +0530

    Added changes to doctorservice

--- src/main/kotlin/com/perfm/finddoctorapp/service/DoctorServiceImpl.kt
+++ src/main/kotlin/com/perfm/finddoctorapp/service/DoctorServiceImpl.kt
@@ -4,6 +4,7 @@ import com.perfm.finddoctorapp.model.Doctor
 import com.perfm.finddoctorapp.repository.DoctorRepository
 import com.perfm.finddoctorapp.repository.HospitalDetailsRepository
 import com.perfm.finddoctorapp.util.BasicCrud
+import org.slf4j.LoggerFactory
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
@@ -11,11 +12,15 @@ import java.util.*
 
 @Service
 class DoctorServiceImpl(val doctorRepository: DoctorRepository, val hospitalDetailsRepository: HospitalDetailsRepository):BasicCrud<String, Doctor> {
+
+    private val log = LoggerFactory.getLogger(DoctorServiceImpl::class.java)
     override fun getAll(pageable: Pageable): Page<Doctor> {
+        log.debug("Inside DoctorServiceImpl::getAll()")
         return doctorRepository.findAll(pageable)
     }
 
     override fun getById(id: String): Optional<Doctor> {
+        log.debug("Inside DoctorServiceImpl::getById()")
         return doctorRepository.findById(id)
     }
 
@@ -23,6 +28,7 @@ class DoctorServiceImpl(val doctorRepository: DoctorRepository, val hospitalDeta
 
 
     override fun update(obj: Doctor): Doctor {
+        log.debug("Inside DoctorServiceImpl::update()")
         return if (doctorRepository.existsById(obj.id))
             doctorRepository.save(obj.apply { this.hospitalAffiliation.hospitalDetails = hospitalDetailsRepository.findById(obj.hospitalAffiliation.hospitalDetails.id).get() })
         else
@@ -30,10 +36,12 @@ class DoctorServiceImpl(val doctorRepository: DoctorRepository, val hospitalDeta
     }
 
     override fun deleteById(id: String): Optional<Doctor> {
+        log.debug("Inside DoctorServiceImpl::deleteById()")
         return doctorRepository.findById(id).apply { this.ifPresent{doctorRepository.delete(it)} }
     }
 
     fun deleteAllDoctorCollections(){
+        log.debug("Inside DoctorServiceImpl::deleteAllDoctorCollections()")
         doctorRepository.deleteAll()
     }
 
